3c8b873cc478178918c287424876c2fe7297e547
Alberto <snk_snake@hotmail.com>
2018-09-26 22:38:47 +0200

    Prueba de nuevo commit

--- app/src/main/java/maa/firebase1/MainActivity.java
+++ app/src/main/java/maa/firebase1/MainActivity.java
@@ -1,7 +1,9 @@
 package maa.firebase1;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("COMMIT", "Prueba de commit");
     }
 }
