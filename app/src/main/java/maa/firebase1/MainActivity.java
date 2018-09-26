9d187fd9e7d6e5c71498908cd44daaadcce0617e
Alberto <snk_snake@hotmail.com>
2018-09-26 22:41:54 +0200

    Prueba de nuevo commit

--- app/src/main/java/maa/firebase1/MainActivity.java
+++ app/src/main/java/maa/firebase1/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("COMMIT", "Prueba de commit");
+        Log.d("COMMIT", "Nuevo");
     }
 }
