f0f71b420f8144a1fc65f29383a0bed978a2861a
Alberto <snk_snake@hotmail.com>
2018-09-30 22:09:29 +0200

    Registro con Gmail y Correo electronico

--- app/src/main/java/maa/firebase1/MainActivity.java
+++ app/src/main/java/maa/firebase1/MainActivity.java
@@ -1,18 +1,101 @@
 package maa.firebase1;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSign, btnRegister, btnSignUi, tryregister;
+    EditText password, username;
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
 
-        Log.d("COMMIT", "Prueba de commit");
-        Log.d("COMMIT", "Nuevo");
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSign = findViewById(R.id.signin);
+        btnRegister = findViewById(R.id.register);
+        btnSignUi = findViewById(R.id.signinui);
+        tryregister = findViewById(R.id.tryregister);
+        password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
+
+
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+    private void login(){
+        try {
+            FirebaseUser usuario = mAuth.getCurrentUser();
+
+            if (usuario != null) {
+                Toast.makeText(this, getText(R.string.session_message), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                Intent.FLAG_ACTIVITY_NEW_TASK |
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            } else {
+                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.Builder(EmailAuthProvider.PROVIDER_ID).build(),
+                        new AuthUI.IdpConfig.Builder(GoogleAuthProvider.PROVIDER_ID).build())).setIsSmartLockEnabled(false).build(), RC_SIGN_IN);
+            }
+        }catch (Exception ex){
+            Log.d("Error", ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == ResultCodes.OK) {
+                login();
+                finish();
+            } else {
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if (response == null) {
+                    Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No estas conectado", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, "Error desconocido", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
     }
 }
